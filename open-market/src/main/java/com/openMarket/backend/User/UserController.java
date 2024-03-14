8db05edfe8054880440a9710b83c46e0341c5f5a
HwanheeMoon <yuiop0218@naver.com>
2024-03-14 15:59:27 +0900

    USER, Security, Redis : 03.14

--- open-market/src/main/java/com/openMarket/backend/User/UserController.java
+++ open-market/src/main/java/com/openMarket/backend/User/UserController.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
